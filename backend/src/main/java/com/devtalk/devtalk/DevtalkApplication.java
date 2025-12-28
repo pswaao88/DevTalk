32f65500a69d73a665eea29fb43ec8b029d032fe
pswaao88 <pswaao88@gmail.com>
2025-12-29 01:10:52 +0900

    init: 스프링 부트 설정

---
+++ backend/src/main/java/com/devtalk/devtalk/DevtalkApplication.java
@@ -0,0 +1,13 @@
+package com.devtalk.devtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtalkApplication.class, args);
+	}
+
+}
