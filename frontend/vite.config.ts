1ecb942356a68775ee4ff5a1468ba994cf4b3835
pswaao88 <pswaao88@gmail.com>
2026-01-04 13:44:46 +0900

    feat: 로컬 기준 포트번호 5174로 고정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174, // 여기에 원하는 포트 번호를 입력하세요
+    strictPort: true, // 포트가 이미 사용 중일 때 자동으로 다음 포트로 넘어가는 것을 방지 (선택 사항)
+  }
 })
