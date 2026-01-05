5cbfa18c980d113b4c119c6c9c9f8b3f43819307
pswaao88 <pswaao88@gmail.com>
2026-01-05 17:46:26 +0900

    feat: Message service 계층 작성

---
+++ backend/src/main/java/com/devtalk/devtalk/service/devtalk/message/MessageService.java
@@ -0,0 +1,39 @@
+package com.devtalk.devtalk.service.devtalk.message;
+
+import com.devtalk.devtalk.domain.devtalk.message.Message;
+import com.devtalk.devtalk.domain.devtalk.message.MessageRepository;
+import com.devtalk.devtalk.domain.devtalk.session.Session;
+import com.devtalk.devtalk.domain.devtalk.session.SessionRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+    private final MessageRepository messageRepository;
+    private final SessionRepository sessionRepository;
+
+    public MessageService(MessageRepository messageRepository, SessionRepository sessionRepository){
+        this.messageRepository = messageRepository;
+        this.sessionRepository = sessionRepository;
+    }
+
+    public Message append(String sessionId, Message message){
+        verifySession(sessionId);
+        return messageRepository.append(sessionId, message);
+    }
+
+    public List<Message> getAll(String sessionId){
+        verifySession(sessionId);
+        return messageRepository.findAllBySessionId(sessionId);
+    }
+
+    public void deleteAll(String sessionId){
+        verifySession(sessionId);
+        messageRepository.deleteAllBySessionId(sessionId);
+    }
+
+    private Session verifySession(String sessionId){
+        return sessionRepository.findById(sessionId)
+            .orElseThrow(() -> new IllegalArgumentException("session not found"));
+    }
+}
