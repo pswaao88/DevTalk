61fd235e7975f88e0186ed141e2002c8d5eb37b4
pswaao88 <pswaao88@gmail.com>
2026-01-09 17:49:42 +0900

    feat: 명세서에 적힌 api 구현
    세션의 모든 메세지 리턴로직 추가
    고옹 url 변경

--- backend/src/main/java/com/devtalk/devtalk/api/controller/devtalk/message/MessageController.java
+++ backend/src/main/java/com/devtalk/devtalk/api/controller/devtalk/message/MessageController.java
@@ -2,7 +2,9 @@ package com.devtalk.devtalk.api.controller.devtalk.message;
 
 import com.devtalk.devtalk.domain.devtalk.message.Message;
 import com.devtalk.devtalk.service.devtalk.message.MessageService;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/devtalk")
+@RequestMapping("/api/devtalk/sessions")
 public class MessageController {
     private final MessageService messageService;
 
@@ -18,9 +20,14 @@ public class MessageController {
         this.messageService = messageService;
     }
 
-    @PostMapping("/sessions/{sessionId}/messages")
+    @PostMapping("/{sessionId}/messages")
     public ResponseEntity<Message> sendMessage(@PathVariable("sessionId")String sessionId, @RequestBody Message message){
         Message saved = messageService.append(sessionId, message);
         return ResponseEntity.ok(saved);
+    }
+
+    @GetMapping("/{sessionId}/messages")
+    public ResponseEntity<List<Message>> getAllMessage(@PathVariable("sessionId")String sessionId){
+        return ResponseEntity.ok(messageService.getAll(sessionId));
     }
 }
