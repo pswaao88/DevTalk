c95d38ffdc5fc18112840a50f4e1dca2f80b4848
pswaao88 <pswaao88@gmail.com>
2026-01-16 10:34:44 +0900

    feat: resolve후 채팅 동기화를 위한 새로운 dto

---
+++ backend/src/main/java/com/devtalk/devtalk/api/dto/response/ResolveWithMessageResponse.java
@@ -0,0 +1,16 @@
+package com.devtalk.devtalk.api.dto.response;
+
+import com.devtalk.devtalk.domain.devtalk.message.Message;
+import com.devtalk.devtalk.domain.devtalk.session.Session;
+
+public record ResolveWithMessageResponse (
+    ResolveResponse resolve,
+    MessageResponse systemMessage
+){
+    public static ResolveWithMessageResponse from(Session session, Message systemMessage){
+        return new ResolveWithMessageResponse(
+            ResolveResponse.from(session),
+            MessageResponse.from(systemMessage)
+        );
+    }
+}
