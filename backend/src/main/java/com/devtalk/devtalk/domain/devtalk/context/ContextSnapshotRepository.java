6c76c5650f6e8db2f5e151d09be9a348c6bd3ad6
pswaao88 <pswaao88@gmail.com>
2026-01-19 17:47:59 +0900

    delete: 컨텍스트 로직 변경으로 필요없는 파일 삭제

--- backend/src/main/java/com/devtalk/devtalk/domain/devtalk/context/ContextSnapshotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devtalk.devtalk.domain.devtalk.context;
-
-public interface ContextSnapshotRepository {
-    String find(String sessionId);
-    String save(String sessionId, String snapshotText);
-    void delete(String sessionId);
-}
